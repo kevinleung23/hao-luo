36e2f1761301b135af1afc83aa46ce97f2e7adbc
Kevin Leung <keleung@microsoft.com>
2017-12-07 15:48:13 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+/*
+ * Proud owner of hao-luo NPM package. All rights reserved.
+ */
+
+// Import required engineers
+import KevinLeung from 'kevin-leung'
+
+// Do something not so impressive
+const haoLuo = () => {
+  return KevinLeung() + 'is great.'
+}
+
+export default haoLuo
